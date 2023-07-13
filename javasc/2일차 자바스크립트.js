dcf9723c902a286eef0d023f44623a0f8cbfb1c5
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-07-13 10:35:57 +0900

    조건문 if,else if,else if,else if,else if,else if,else

---
+++ javasc/2일차 자바스크립트.js
@@ -0,0 +1,22 @@
+console.log(50);
+console.log(50-3);
+
+const a =50;
+let b =50;
+a=b;
+a=50000;
+console(a,b);
+console.log("안녕하세요");
+
+var date= new Date();
+var hour = date.getHours();
+
+if (hour>11){
+    alert('알림')
+}
+else if (hour<15){
+    alert('점심')
+}
+else{
+    alert('저녁')
+}
