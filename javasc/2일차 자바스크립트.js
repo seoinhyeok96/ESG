6c294985c1f3dcf345cd208e724ccd951412bb53
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-07-13 10:41:07 +0900

    switch문 추가

--- javasc/2일차 자바스크립트.js
+++ javasc/2일차 자바스크립트.js
@@ -20,3 +20,16 @@ else if (hour<15){
 else{
     alert('저녁')
 }
+
+var input = Number(prompt('숫자를 입력하세요','숫자'));
+switch(input){
+    case 0:
+        alert('짝수입니다.');
+        break;
+    case 1:
+        alert('홀수입니다.');
+        break;
+    default:
+        alert('숫자가 아닙니다')
+        break;
+    }
