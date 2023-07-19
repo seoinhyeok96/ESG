27d3d10106091aea327b936c0a1ee05e752ef73a
Thinking Seo <110218894+seoinhyeok96@users.noreply.github.com>
2023-07-19 09:17:28 +0900

    commit

--- 3일차/문제1.js
+++ 3일차/문제1.js
@@ -4,3 +4,10 @@ let date = [
 {회원번호: 2, 아이디: 'green',성별:'여자','나이':16}
 {회원번호: 3, 아이디: 'orane',성별 : '여자','나이':15}
 ];
+
+
+for (let element fo date) {
+    if(element.gender === '남자'&& element.age >20) {
+     console.log('성별은 ${element.gender}   
+    }
+}
